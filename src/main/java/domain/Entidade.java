58c763cf0d36143aeff6f4161e002d1b8d0f740a
ThiagoLeite <thiagoflosino@gmail.com>
2018-01-20 02:58:28 -0200

    aaaa

--- src/main/java/domain/Entidade.java
+++ src/main/java/domain/Entidade.java
@@ -1,47 +1,36 @@
 package domain;
 
-import java.util.List;
-
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
-import org.neo4j.ogm.annotation.Property;
-import org.neo4j.ogm.annotation.Relationship;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
-@NodeEntity
+@NodeEntity(label = "Entidade")
 public abstract class Entidade {
 		
 	@Id @GeneratedValue
 	private Long id;
 	
-	@Property(name="posicao")
 	private String posicao;
 	
-	@Property(name="trecho")
 	private String trecho;
 	
-	@Property(name="numeroCitacoes")
 	private Integer numeroCitacoes;
 	
-	@Property(name="numeroRelacoes")
 	private Integer numeroRelacoes;
-	
-	@Property(name="tipoEntidade")
-	private String tipoEntidade;
-	
-	@Relationship(direction = Relationship.INCOMING)
-	private List<ArestaDocumento> documentos;
-	
+
+	public Entidade() {
 		
-	public Entidade(Long id, String posicao, String techo, Integer numeroCitacoes, Integer numeroRelacoes) {
+	}
+	
+	public Entidade(Long id, String posicao, String trecho, Integer numeroCitacoes, Integer numeroRelacoes) {
 		super();
 		this.id = id;
 		this.posicao = posicao;
-		this.trecho = techo;
+		this.trecho = trecho;
 		this.numeroCitacoes = numeroCitacoes;
 		this.numeroRelacoes = numeroRelacoes;
 	}
@@ -86,20 +75,5 @@ public abstract class Entidade {
 		this.numeroRelacoes = numeroRelacoes;
 	}
 
-	public List<ArestaDocumento> getDocumentos() {
-		return documentos;
-	}
-
-	public void setDocumentos(List<ArestaDocumento> documentos) {
-		this.documentos = documentos;
-	}
-
-	public String getTipoEntidade() {
-		return tipoEntidade;
-	}
-
-	public void setTipoEntidade(String tipoEntidade) {
-		this.tipoEntidade = tipoEntidade;
-	}
 
 }
---
+++ src/main/java/domain/TipoProcesso.java
@@ -0,0 +1,7 @@
+package domain;
+
+public enum TipoProcesso {
+	
+	PROGRESÃO, AFASTAMENTO;
+
+}
